fdf4d17560b81fc4d27be1b276b2b2fc0d7e1f8f
Vinil Dev Studio <adriano@vinil.co>
2025-06-10 09:51:00 -0300

    Refactor page-sobre.php to replace static content with dynamic content using the_content() function, enhancing maintainability and flexibility of the template.

--- wp-content/themes/luanaraujo-hml1/template-parts/content/page-sobre.php
+++ wp-content/themes/luanaraujo-hml1/template-parts/content/page-sobre.php
@@ -19,72 +19,9 @@
                                          src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-la-sobre.png?v=1742321523"
                                          loading="lazy" alt="">
                                     <div class="spacer-small"></div>
-                                    <p class="text-size-medium">Lorem ipsum dolor sit amet consectetur. Sed pretium nibh
-                                        iaculis euismod quam vel diam. In in pellentesque vel eu consequat. Diam quis
-                                        pharetra scelerisque aliquet etiam pretium hendrerit nisl. Tristique dolor
-                                        accumsan sollicitudin pellentesque vestibulum amet. Tellus a etiam tortor
-                                        blandit sed. Velit aenean blandit ultricies at sit pellentesque tellus dolor
-                                        vel. Facilisi scelerisque pharetra metus at vulputate nunc vivamus.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="spacer-huge"></div>
-                        <div class="layout3_component">
-                            <div class="w-layout-grid layout3_content">
-                                <div class="layout3_content-left">
-                                    <h2 class="heading-sobre heading-style-h3">Long heading is what you see here in this
-                                        feature section</h2>
-                                    <div class="spacer-small"></div>
-                                    <div class="text-rich-text w-richtext">
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                                            cillum dolore eu fugiat nulla pariatur.</p>
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                                            cillum dolore eu fugiat nulla pariatur.</p>
+                                    <div class="text-rich-text">
+                                        <?php the_content(); ?>
                                     </div>
-                                </div>
-                                <div data-delay="4000" data-animation="slide" class="gallery15_slider w-slider"
-                                     data-autoplay="false" data-easing="ease" data-hide-arrows="false"
-                                     data-disable-swipe="false" data-autoplay-limit="0" data-nav-spacing="3"
-                                     data-duration="500" data-infinite="false">
-                                    <div class="gallery15_mask w-slider-mask">
-                                        <div class="gallery15_slide w-slide">
-                                            <div class="gallery15_image-wrapper"><img loading="lazy"
-                                                     src="<?php echo get_template_directory_uri(); ?>/assets/images/placeholder-image.svg?v=1742321523"
-                                                     alt="" class="gallery15_image"></div>
-                                        </div>
-                                        <div class="gallery15_slide w-slide">
-                                            <div class="gallery15_image-wrapper"><img loading="lazy"
-                                                     src="<?php echo get_template_directory_uri(); ?>/assets/images/placeholder-image.svg?v=1742321523"
-                                                     alt="" class="gallery15_image"></div>
-                                        </div>
-                                        <div class="gallery15_slide w-slide">
-                                            <div class="gallery15_image-wrapper"><img loading="lazy"
-                                                     src="<?php echo get_template_directory_uri(); ?>/assets/images/placeholder-image.svg?v=1742321523"
-                                                     alt="" class="gallery15_image"></div>
-                                        </div>
-                                    </div>
-                                    <div
-                                         class="slider-arrow is-centre-previous hide-mobile-landscape w-slider-arrow-left">
-                                        <div class="slider-arrow-icon_default w-embed"><svg width="30" height="24"
-                                                 viewBox="0 0 30 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M1.25267 10.233C0.80323 10.7017 0.550783 11.3371 0.550783 11.9996C0.550782 12.6622 0.803229 13.2976 1.25267 13.7663L10.3023 23.198C10.7525 23.667 11.3631 23.9305 11.9999 23.9305C12.6366 23.9305 13.2472 23.667 13.6975 23.198C14.1477 22.729 14.4006 22.0929 14.4006 21.4297C14.4006 20.7664 14.1477 20.1303 13.6975 19.6613L8.74387 14.4996L27.1999 14.4997C27.8364 14.4997 28.4468 14.2363 28.8969 13.7674C29.347 13.2986 29.5999 12.6627 29.5999 11.9997C29.5999 11.3366 29.347 10.7007 28.8969 10.2319C28.4468 9.76304 27.8364 9.49965 27.1999 9.49965L8.74387 9.49965L13.6975 4.33965C13.9204 4.10743 14.0972 3.83174 14.2179 3.52833C14.3385 3.22492 14.4006 2.89973 14.4006 2.57132C14.4006 2.24291 14.3385 1.91771 14.2179 1.6143C14.0972 1.31089 13.9204 1.0352 13.6975 0.80298C13.4745 0.570761 13.2099 0.386555 12.9186 0.260878C12.6273 0.1352 12.3151 0.0705145 11.9999 0.0705145C11.6846 0.0705144 11.3724 0.1352 11.0811 0.260877C10.7899 0.386554 10.5252 0.57076 10.3023 0.80298L1.25267 10.233Z"
-                                                      fill="#723A30"></path>
-                                            </svg></div>
-                                    </div>
-                                    <div class="slider-arrow is-centre-next hide-mobile-landscape w-slider-arrow-right">
-                                        <div class="slider-arrow-icon_default w-embed"><svg width="30" height="24"
-                                                 viewBox="0 0 30 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M28.7473 13.767C29.1968 13.2983 29.4492 12.6628 29.4492 12.0003C29.4492 11.3378 29.1968 10.7024 28.7473 10.2337L19.6977 0.802002C19.2475 0.333011 18.6369 0.0695337 18.0001 0.0695336C17.3634 0.0695336 16.7528 0.33301 16.3025 0.802002C15.8523 1.27099 15.5994 1.90708 15.5994 2.57033C15.5994 3.23359 15.8523 3.86968 16.3025 4.33867L21.2561 9.50034L2.80013 9.50034C2.16361 9.50034 1.55316 9.76373 1.10307 10.2326C0.652984 10.7014 0.400127 11.3373 0.400127 12.0003C0.400127 12.6634 0.652984 13.2993 1.10307 13.7681C1.55316 14.2369 2.16361 14.5003 2.80013 14.5003L21.2561 14.5003L16.3025 19.6603C16.0796 19.8926 15.9028 20.1682 15.7821 20.4717C15.6615 20.7751 15.5994 21.1003 15.5994 21.4287C15.5994 21.7571 15.6615 22.0823 15.7821 22.3857C15.9028 22.6891 16.0796 22.9648 16.3025 23.197C16.5255 23.4292 16.7901 23.6134 17.0814 23.7391C17.3727 23.8648 17.6849 23.9295 18.0001 23.9295C18.3154 23.9295 18.6276 23.8648 18.9189 23.7391C19.2101 23.6134 19.4748 23.4292 19.6977 23.197L28.7473 13.767Z"
-                                                      fill="#723A30"></path>
-                                            </svg></div>
-                                    </div>
-                                    <div class="gallery15_slide-nav w-slider-nav w-slider-nav-invert w-round"></div>
                                 </div>
                             </div>
                         </div>
