d90c663be6df3900ca9deb84830be3296f3c3388
Vinil Dev Studio <adriano@vinil.co>
2025-08-27 16:23:05 -0300

    Add tracking script to header and update header arguments for improved analytics integration.

--- wp-content/themes/luanaraujo-hml1/header.php
+++ wp-content/themes/luanaraujo-hml1/header.php
@@ -5,6 +5,7 @@ defined("ABSPATH") || exit(); ?>
 <?php $args = wp_parse_args($args, [
 	"wfPage" => "67d48426a6d9dae6ce6f3a09",
 	"head" => "head/front-page",
+	"trackoing" => "head/tracking",
 	"body" => "",
 ]); ?>
 <html data-wf-page="<?php echo $args["wfPage"]; ?>" data-wf-site="67d48425a6d9dae6ce6f39c9"
@@ -12,6 +13,7 @@ defined("ABSPATH") || exit(); ?>
 
 <head><?php
 get_template_part("template-parts/" . $args["head"]);
+get_template_part("template-parts/" . $args["tracking"]);
 wp_enqueue_script("jquery");
 wp_head();
 ?></head>
---
+++ wp-content/themes/luanaraujo-hml1/template-parts/head/tracking.php
@@ -0,0 +1,2 @@
+<script defer src="https://unami-jupiter-app.vercel.app/script.js"
+        data-website-id="34c65514-f4ab-4f39-a206-22829d48eeba"></script>
