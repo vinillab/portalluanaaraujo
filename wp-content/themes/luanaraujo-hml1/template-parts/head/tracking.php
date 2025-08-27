c6674788d2550cb139abfd4e4e906be374740b3e
Vinil Dev Studio <adriano@vinil.co>
2025-08-27 18:04:33 -0300

    Add Google Tag Manager and Umami Analytics scripts to tracking.php for enhanced analytics capabilities, and include UOL Code for additional tracking functionality.

--- wp-content/themes/luanaraujo-hml1/template-parts/head/tracking.php
+++ wp-content/themes/luanaraujo-hml1/template-parts/head/tracking.php
@@ -1,2 +1,26 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-X8PBC6SV1F"></script>
+<script>
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+    dataLayer.push(arguments);
+}
+gtag('js', new Date());
+
+gtag('config', 'G-X8PBC6SV1F');
+</script>
+
+<!-- Umami Analytics -->
 <script defer src="https://unami-jupiter-app.vercel.app/script.js"
         data-website-id="34c65514-f4ab-4f39-a206-22829d48eeba"></script>
+
+<!--UOL Code -->
+<script type="text/javascript">
+(function() {
+    window.universal_variable = window.universal_variable || {};
+    window.universal_variable.dfp = window.universal_variable.dfp || {};
+    window.uolads = window.uolads || [];
+})();
+</script>
+<script type="text/javascript" src="//tm.jsuol.com.br/uoltm.js?id=03yh0r" async></script>
